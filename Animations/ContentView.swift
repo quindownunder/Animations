b5682e4ce2ff058bfba95fb342752524d3e5e657
Matthew Richardson <mat_richardson@mac.com>
2020-08-16 12:03:22 +1000

    Initial Version

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -9,8 +9,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var animationAmount: CGFloat = 1
+    
     var body: some View {
-        Text("Hello, World!")
+        Button("Tap Me") {
+            self.animationAmount += 1
+        }
+        .padding(50)
+        .background(Color.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .scaleEffect(animationAmount)
+        .animation(
+            Animation.easeInOut(duration: 1)
+                .repeatCount(3, autoreverses: true)
+        )
     }
 }
 
