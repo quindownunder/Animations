b8be84fe2ad31d7a3705434097bc367d819ae10c
Matthew Richardson <mat_richardson@mac.com>
2020-08-18 17:30:42 +1000

    Animation binding

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -9,25 +9,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    
     @State private var animationAmount: CGFloat = 1
-    
+
     var body: some View {
-        Button("Tap Me") {
-            self.animationAmount += 1
-        }
-        .padding(50)
-        .background(Color.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .scaleEffect(animationAmount)
-        .animation(
-            Animation.easeInOut(duration: 1)
+        print(animationAmount)
+        return VStack {
+            Stepper("Scale amount \(animationAmount)", value: $animationAmount.animation(
+                Animation.easeInOut(duration: 1)
                 .repeatCount(3, autoreverses: true)
-        )
+            ), in: 1...10)
+
+            Spacer()
+
+            Button("Tap Me") {
+                self.animationAmount += 1
+            }
+            .padding(40)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
