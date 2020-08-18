ad40d26054b50b7d037b97aa8e70a334d1cfd727
Matthew Richardson <mat_richardson@mac.com>
2020-08-18 17:31:22 +1000

    Animation on appear

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -9,29 +9,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     @State private var animationAmount: CGFloat = 1
-
+    
     var body: some View {
-        print(animationAmount)
-        return VStack {
-            Stepper("Scale amount \(animationAmount)", value: $animationAmount.animation(
-                Animation.easeInOut(duration: 1)
-                .repeatCount(3, autoreverses: true)
-            ), in: 1...10)
-
-            Spacer()
-
-            Button("Tap Me") {
-                self.animationAmount += 1
-            }
-            .padding(40)
-            .background(Color.red)
-            .foregroundColor(.white)
-            .clipShape(Circle())
-            .scaleEffect(animationAmount)
+        Button("Tap Me") {
+            // self.animationAmount += 1
+        }
+        .padding(40)
+        .background(Color.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .overlay(
+            Circle()
+                .stroke(Color.red)
+                .scaleEffect(animationAmount)
+                .opacity(Double(2 - animationAmount))
+                .animation(
+                    Animation.easeOut(duration: 1)
+                        .repeatForever(autoreverses: false)
+                )
+        )
+        .onAppear {
+            self.animationAmount = 2
         }
     }
 }
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
